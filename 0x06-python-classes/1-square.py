380db73b669df393cf7456f85f983e4749bcdce8
Ola2toni <davidolawuyi2003@gmail.com>
2023-01-02 07:56:33 -0800

    Task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,8 +8,6 @@ class Square:
 
     def __init__(self, size):
     """Initializing this square class
-    Args: size - represnets the size of the square defined
-                                        """
-
-                                        self.__size = size
+    Args: size - represnets the size of the square defined"""
+    self.__size = size
                                                 
